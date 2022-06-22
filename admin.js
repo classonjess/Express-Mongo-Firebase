7d66b47bfd252b860dc24c7d64fdde43f4562393
Jessica Classon <jessicaclasson4@gmail.com>
2022-06-22 12:18:33 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ var serviceAccount = require("path/to/serviceAccountKey.json");
 // Firebase service account page
 {
     const type: "service_account",
-    const project_id: "adding-authentication-3ef37",
+    const project_id: "",
     const private_key_id: ,
     const private_key: "-----BEGIN PRIVATE KEY-----\,
     const client_id: "",
